8a4e1c85b58ef5689e4ecdeda558ce74effb2f51
AntonioL <surferparadisetony@gmail.com>
2020-06-02 15:56:22 +0200

    new

--- Python-app/test.py
+++ Python-app/test.py
@@ -1,15 +1,18 @@
-from sendmail import MailSender
+import smtplib
+from email.mime.text import MIMEText
 
-def main():
+smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+smtp_ssl_port = 465
+username = 'suferparadisetony@gmail.com'
+password = '!Tony12089'
+sender = 'suferparadisetony@gmail.com'
+targets = ['antoniol@supermicro.com']
 
-    plaintext = "Hello John, \n" \
-                "I'm just testing my new fancypants email sending system here.\n" \
-                "Antonio"
-    ourmailsender = MailSender('suferparadise@gmail.com', '!Tony12089', ('smtp.gmail.com', 587))
-    ourmailsender.set_message(plaintext, "This is a test", "Adam Adamson")
-    ourmailsender.set_recipients(['antoniol@supermicro.com'])
-    ourmailsender.connect()
-    ourmailsender.send_all()
+msg = MIMEText('Hi, how are you today?')
+msg['Subject'] = 'Hello'
+msg['From'] = sender
+msg['To'] = ', '.join(targets)
 
-if __name__ == '__main__':
-    main()
+server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+server.login(username, password)
+server.sendmail(sender, targets, msg.as_string())
