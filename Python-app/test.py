b0ef41ec2746a1f5d4ae21c1ecb554943d450b26
AntonioL <surferparadisetony@gmail.com>
2020-06-02 15:12:45 +0200

    new

--- Python-app/test.py
+++ Python-app/test.py
@@ -1,6 +1,15 @@
+from sendmail import MailSender
 
 def main():
-    print("checking hello world")
+
+    plaintext = "Hello John, \n" \
+                "I'm just testing my new fancypants email sending system here.\n" \
+                "Antonio"
+    ourmailsender = MailSender('suferparadise@gmail.com', '!Tony12089', ('smtp.gmail.com', 587))
+    ourmailsender.set_message(plaintext, "This is a test", "Adam Adamson")
+    ourmailsender.set_recipients(['antoniol@supermicro.com'])
+    ourmailsender.connect()
+    ourmailsender.send_all()
 
 if __name__ == '__main__':
     main()
