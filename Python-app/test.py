46f7a9cdb638267994d9da6739cc70c3806dc933
AntonioL <surferparadisetony@gmail.com>
2020-06-03 13:19:26 +0200

    new

--- Python-app/test.py
+++ Python-app/test.py
@@ -1,31 +1,22 @@
 import smtplib
-import ssl
 from email.mime.text import MIMEText
 
 def send():
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'suferparadisetony@gmail.com'
-    EMAIL_HOST_PASSWORD = '!Tony12089'
-    EMAIL_PORT = 587
+    smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+    smtp_ssl_port = 465
+    username = 'suferparadisetony@gmail.com'
+    password = '!Tony12089'
+    sender = 'suferparadisetony@gmail.com'
+    targets = ['antoniol@supermicro.com']
 
-    port = EMAIL_PORT
-    smtp_server = EMAIL_HOST
-    sender_email = EMAIL_HOST_USER
-    password = EMAIL_HOST_PASSWORD
-    receiver_email = 'antoniol@supermicro.com'
-    subject = 'HelloHellow123'
-    body = 'get get a ticket.'
-    message = 'Subject: {}\n\n{}'.format(subject, body)
-    context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.ehlo()  # Can be omitted
-        server.starttls(context=context)
-        server.ehlo()  # Can be omitted
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-    return
+    msg = MIMEText('Hi, how are you today?')
+    msg['Subject'] = 'Hello'
+    msg['From'] = sender
+    msg['To'] = ', '.join(targets)
+
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    server.login(username, password)
+    server.sendmail(sender, targets, msg.as_string())
 
 def main():
     send()
