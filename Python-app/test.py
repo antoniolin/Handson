042a62e93efa6d116e4071d3ce5ef23bd4eaa933
AntonioL <surferparadisetony@gmail.com>
2020-06-03 12:36:53 +0200

    new

--- Python-app/test.py
+++ Python-app/test.py
@@ -1,18 +1,19 @@
 import smtplib
 from email.mime.text import MIMEText
 
-smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
-smtp_ssl_port = 465
-username = 'suferparadisetony@gmail.com'
-password = '!Tony12089'
-sender = 'suferparadisetony@gmail.com'
-targets = ['antoniol@supermicro.com']
+def send():
+    smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+    smtp_ssl_port = 465
+    username = 'suferparadisetony@gmail.com'
+    password = '!Tony12089'
+    sender = 'suferparadisetony@gmail.com'
+    targets = ['antoniol@supermicro.com']
 
-msg = MIMEText('Hi, how are you today?')
-msg['Subject'] = 'Hello'
-msg['From'] = sender
-msg['To'] = ', '.join(targets)
+    msg = MIMEText('Hi, how are you today?')
+    msg['Subject'] = 'Hello'
+    msg['From'] = sender
+    msg['To'] = ', '.join(targets)
 
-server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
-server.login(username, password)
-server.sendmail(sender, targets, msg.as_string())
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    server.login(username, password)
+    server.sendmail(sender, targets, msg.as_string())
